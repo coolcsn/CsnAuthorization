29cec2ab74018d8f3c0de494b9e0bc86c8c14760
Stoyan Revov <st.revov@gmail.com>
2013-09-10 01:48:55 +0300

    Documetation blocks added.

--- Module.php
+++ Module.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * coolcsn
+ * @link https://github.com/coolcsn/CsnAuthorization for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 LightSoft 2005 Ltd. Bulgaria
+ * @license https://github.com/coolcsn/CsnAuthorization/blob/master/LICENSE BSDLicense
+ * @author Stoyan Cheresharov <stoyan@coolcsn.com>, Stoyan Revov <st.revov@gmail.com>
+*/
+
+/**
  * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
  * It allows usage of this module even without composer.
  * The original Module.php is in 'src/CsnAuthorization' in order to respect PSR-0
--- src/CsnAuthorization/Module.php
+++ src/CsnAuthorization/Module.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * coolcsn
+ * @link https://github.com/coolcsn/CsnAuthorization for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 LightSoft 2005 Ltd. Bulgaria
+ * @license https://github.com/coolcsn/CsnAuthorization/blob/master/LICENSE BSDLicense
+ * @author Stoyan Cheresharov <stoyan@coolcsn.com>, Stoyan Revov <st.revov@gmail.com>
+*/
 
 namespace CsnAuthorization;
 
