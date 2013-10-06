6011e98eb77c4fae70fd8f75a75bc4244f9b68d4
Stoyan Revov <st.revov@gmail.com>
2013-10-06 13:21:46 +0300

    Resource entity moved from CsnCms.

--- config/module.config.php
+++ config/module.config.php
@@ -8,4 +8,23 @@
  * @author Stoyan Cheresharov <stoyan@coolcsn.com>, Stoyan Revov <st.revov@gmail.com>
 */
 
-return array();
+namespace CsnAuthorization;
+
+return array(
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity',
+                ),
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver',
+                )
+            )
+        )
+    ),
+);
---
+++ src/CsnAuthorization/Entity/Resource.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Coolcsn Zend Framework 2 Authorization Module
+ * 
+ * @link https://github.com/coolcsn/CsnAuthorization for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 LightSoft 2005 Ltd. Bulgaria
+ * @license https://github.com/coolcsn/CsnAuthorization/blob/master/LICENSE BSDLicense
+ * @author Stoyan Cheresharov <stoyan@coolcsn.com>
+ * @author Stoyan Revov <st.revov@gmail.com>
+*/
+
+namespace CsnAuthorization\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Zend\Form\Annotation; // !!!! Absolutely neccessary
+
+/**
+ * Resources
+ *
+ * @ORM\Table(name="resource")
+ * @ORM\Entity
+ * @Annotation\Name("resource")
+ * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ClassMethods")
+ */
+class Resource
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100, nullable=false)
+     * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":30}})
+     * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[a-zA-Z][a-zA-Z0-9_-]{0,100}$/"}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Resource:"})
+     */
+    protected $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @Annotation\Exclude()
+     */
+    protected $id;
+
+    /**
+     * Set name
+     *
+     * @param  string   $name
+     * @return Resource
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
